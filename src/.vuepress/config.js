3208291b4841e5501858d84af90eab06c9a67c68
F170502NT01\ikuko <ikuko@fixstars.com>
2019-12-13 15:09:20 +0900

    The first commit

---
+++ src/.vuepress/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+   title: 'GitHub Pages product by VuePress',
+   description: 'I am learning publish VuePress with GitHub Pages',
+   dest: 'docs/',
+   base: '/vue-press/',
+ }
