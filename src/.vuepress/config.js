9975ed9f897049dc14478e810b2acf526a92e56c
F170502NT01\ikuko <ikuko@fixstars.com>
2019-12-15 07:13:09 +0900

    restore cache

---
+++ src/.vuepress/config.js
@@ -0,0 +1,10 @@
+
+const title = 'GitHub Pages by VuePress';
+const description = 'I am learning publish VuePress with GitHub Pages';
+
+module.exports = {
+  title: title,
+  description: description,
+  dest: 'docs/',
+  base: '/vuepress-test/'
+};
